ac798e821d235b8eedcad2bb1e308f993e8f28f8
marlevek <marcelo_levek@hotmail.com>
2024-10-26 10:01:45 -0300

    adicionadas mais fabricantes

--- dados_AC.py
+++ dados_AC.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.set_page_config('Informações para instalação ar-condicionado', page_icon=':mechanic:')
-st.title('Dados Ar-Condicionado :material/heat_pump:')
+st.set_page_config('Informações para instalação ar-condicionado Hi Wall', page_icon=':mechanic:')
+st.title('Características Ar-Condicionado Hi Wall :material/heat_pump:')
 
 parametros = {
 'Agratto': {
@@ -93,9 +93,10 @@ parametros = {
                 'limite_fabrica': 8.0,
                 'limite_minimo': 2.0,
                 'limite_maximo': 15.0,
-                'cabo_pp': '4 x 1,5mm',
-                'ligacao_eletrica': 'condensadora',
-                'tubulacao': '1/4 e 1/2'         
+                'cabo_pp': '4 x 1,5mm (maioria); ou 5 x 1,5mm para os modelos UI/UE (quente e frio)',
+                'ligacao_eletrica': 'Na condensadora para os modelos JI/JE (só frio e quente-frio), YI/YE (só frio e quente-frio) e UI/UE (só frio), e na Evaporadora para os modelos UI/UE (quente-frio)',
+                'tubulacao': '1/4 e 3/8',
+
             }
         },
         '18.000 BTU/h': {
@@ -104,9 +105,9 @@ parametros = {
                 'limite_fabrica': 8.0,
                 'limite_minimo': 2.0,
                 'limite_maximo': 15.0,
-                'cabo_pp': '4 x 1,5mm',
-                'ligacao_eletrica': 'condensadora',
-                'tubulacao': '1/4 e 1/2'       
+               'cabo_pp': '4 x 1,5mm (maioria); ou 5 x 1,5mm para os modelos UI/UE (quente e frio)',
+                'ligacao_eletrica': 'Na condensadora para os modelos JI/JE (só frio e quente-frio), YI/YE (só frio e quente-frio) e UI/UE (só frio), e na Evaporadora para os modelos UI/UE (quente-frio)',
+                'tubulacao': '1/4 e 1/2',   
                 }
         },        
         '24.000 BTU/h': {
@@ -115,8 +116,8 @@ parametros = {
                 'limite_fabrica': 8.0,
                 'limite_minimo': 2.0,
                 'limite_maximo': 15.0,
-                'cabo_pp': '4 x 1,5mm',
-                'ligacao_eletrica': 'condensadora',
+                'cabo_pp': '4 x 1,5mm (maioria); ou 5 x 1,5mm para os modelos UI/UE (quente e frio)',
+                'ligacao_eletrica': 'Na condensadora para os modelos JI/JE (só frio e quente-frio), YI/YE (só frio e quente-frio) e UI/UE (só frio), e na Evaporadora para os modelos UI/UE (quente-frio)',
                 'tubulacao': '1/4 e 1/2'       
             }
         }              
@@ -229,19 +230,102 @@ parametros = {
  },
  
  'Gree': {
-        '9 a 12.000 BTU/h': {
+        '9.000 BTU/h': {
             'R-32':{
-                'carga_por_metro': 15,
+                'carga_por_metro': 12,
                 'limite_fabrica': 5.0,
+                'limite_minimo': 2.0,
+                'limite_maximo': 15.0,
+                'cabo_pp': '4 x 1,5mm',
+                'ligacao_eletrica': 'condensadora',
+                'tubulacao': '1/4 e 3/8',
+                'observacoes': ('Se for modelo só frio, a carga adicional de gás é 12g/metro acima de 5m')    
+        }
+    },
+        '12.000 BTU/h': {
+            'R-32':{
+                'carga_por_metro': 12,
+                'limite_fabrica': 5.0,
+                'limite_minimo': 2.0,
+                'limite_maximo': 20.0,
+                'cabo_pp': '4 x 1,5mm',
+                'ligacao_eletrica': 'condensadora',
+                'tubulacao': '1/4 e 3/8',
+                'observacoes': ('Se for modelo só frio, a carga adicional de gás é 12g/metro acima de 5m')       
+        }
+    },
+        '18.000 BTU/h': {
+            'R-32':{
+                'carga_por_metro': 12,
+                'limite_fabrica': 5.0,
+                'limite_minimo': 2.0,
+                'limite_maximo': 25.0,
+                'cabo_pp': '4 x 1,5mm',
+                'ligacao_eletrica': 'condensadora',
+                'tubulacao': '1/4 e 1/2',
+                'observacoes': ('Se for modelo só frio, a carga adicional de gás é 12g/metro acima de 5m')       
+        }
+    },
+        '24.000 BTU/h': {
+            'R-32':{
+                'carga_por_metro': 20,
+                'limite_fabrica': 5.0,
+                'limite_minimo': 2.0,
+                'limite_maximo': 25.0,
+                'cabo_pp': '4 x 1,5mm',
+                'ligacao_eletrica': 'condensadora',
+                'tubulacao': '1/4 e 1/2'       
+        }
+    },
+},
+ 
+'Hitachi':{
+        '9.000 BTU/h':{
+            'R-32':{
+                'carga_por_metro': 20,
+                'limite_fabrica': 5,
                 'limite_minimo': 3.0,
                 'limite_maximo': 15.0,
                 'cabo_pp': '4 x 1,5mm',
                 'ligacao_eletrica': 'condensadora',
-                'tubulacao': '1/48 e 3/8'       
-        }
-    },
- },
- 
+                'tubulacao': '1/4 e 3/8'
+                }
+        },
+        '12.000 BTU/h':{
+            'R-32':{
+                'carga_por_metro': 20,
+                'limite_fabrica': 5,
+                'limite_minimo': 3.0,
+                'limite_maximo': 15.0,
+                'cabo_pp': '4 x 1,5mm',
+                'ligacao_eletrica': 'condensadora',
+                'tubulacao': '1/4 e 3/8'
+                }
+        },
+        '18.000 BTU/h':{
+            'R-32':{
+                'carga_por_metro': 20,
+                'limite_fabrica': 5.0,
+                'limite_minimo': 3.0,
+                'limite_maximo':30.0 ,
+                'cabo_pp': '4 x 1,5mm',
+                'ligacao_eletrica': 'condensadora',
+                'tubulacao': '1/4 e 1/2'
+                }
+        },
+        '24.000 BTU/h':{
+            'R-32':{
+                'carga_por_metro': 20,
+                'limite_fabrica': 5.0,
+                'limite_minimo': 3.0,
+                'limite_maximo': 30.0,
+                'cabo_pp': '4 x 2,5mm',
+                'ligacao_eletrica': 'condensadora',
+                'tubulacao': '1/4 e 5/8'
+                }
+        },
+},
+
 'LG': {
         '9 a 15.000 BTU/h': {
             'R-32': {
@@ -290,15 +374,26 @@ parametros = {
  },
  
 'Midea':{
-   '9 a 12.000 BTU/h': {
+   '9.000 BTU/h': {
        'R-32': {
             'carga_por_metro': 15,
             'limite_fabrica': 5.0,
             'limite_minimo': 2.0,
-            'limite_maximo': 25.0,
+            'limite_maximo': 15.0,
             'cabo_pp': '4 x 1,5mm',
             'ligacao_eletrica': 'condensadora',
             'tubulacao': '1/4 e 3/8'     
+       }
+   },
+   '12.000 BTU/h': {
+       'R-32': {
+            'carga_por_metro': 15,
+            'limite_fabrica': 5.0,
+            'limite_minimo': 2.0,
+            'limite_maximo': 15.0,
+            'cabo_pp': '4 x 1,5mm',
+            'ligacao_eletrica': 'condensadora',
+            'tubulacao': '1/4 e 1/2'     
        }
    },
    '18.000 BTU/h': {
@@ -306,10 +401,10 @@ parametros = {
             'carga_por_metro': 15,
             'limite_fabrica': 5.0,
             'limite_minimo': 2.0,
-            'limite_maximo': 30.0,
+            'limite_maximo': 15.0,
             'cabo_pp': '4 x 1,5mm',
             'ligacao_eletrica': 'condensadora',
-            'tubulacao': '1/4 e 1/2'     
+            'tubulacao': '1/4 e 1/2'    
        }
    },
    '24.000 BTU/h': {
@@ -320,14 +415,84 @@ parametros = {
             'limite_maximo': 30.0,
             'cabo_pp': '4 x 1,5mm',
             'ligacao_eletrica': 'condensadora',
-            'tubulacao': '3/8 e 5/8'     
+            'tubulacao': '3/8 e 5/8',
+            'observacoes': ('Se o comprimento da tubulação for maior que 25m mudar tubulação da linha de sucção para 5/8')      
        }
    },
  },
 
+'Philco':{
+   '9.000 BTU/h': {
+       'R-32': {
+            'carga_por_metro': 15,
+            'limite_fabrica': 5.0,
+            'limite_minimo': 2.0,
+            'limite_maximo': 15.0,
+            'cabo_pp': '4 x 1,5mm',
+            'ligacao_eletrica': 'condensadora',
+            'tubulacao': '1/4 e 3/8'     
+       }
+   },
+   '12.000 BTU/h': {
+       'R-32': {
+            'carga_por_metro': 15,
+            'limite_fabrica': 5.0,
+            'limite_minimo': 2.0,
+            'limite_maximo': 15.0,
+            'cabo_pp': '4 x 1,5mm',
+            'ligacao_eletrica': 'condensadora',
+            'tubulacao': '1/4 e 3/8'     
+       }
+   },
+   '18.000 BTU/h': {
+       'R-32': {
+            'carga_por_metro': 25,
+            'limite_fabrica': 5.0,
+            'limite_minimo': 2.0,
+            'limite_maximo': 25.0,
+            'cabo_pp': '4 x 1,5mm',
+            'ligacao_eletrica': 'condensadora',
+            'tubulacao': '1/4 e 3/8'    
+       }
+   },
+   '24.000 BTU/h': {
+       'R-32': {
+            'carga_por_metro': 25,
+            'limite_fabrica': 5.0,
+            'limite_minimo': 2.0,
+            'limite_maximo': 25.0,
+            'cabo_pp': '4 x 1,5mm',
+            'ligacao_eletrica': 'condensadora',
+            'tubulacao': '1/4 e 1/2',   
+       }
+   },
+   '30.000 BTU/h': {
+       'R-32': {
+            'carga_por_metro': 25,
+            'limite_fabrica': 5.0,
+            'limite_minimo': 2.0,
+            'limite_maximo': 25.0,
+            'cabo_pp': '4 x 1,5mm',
+            'ligacao_eletrica': 'condensadora',
+            'tubulacao': '1/4 e 1/2',   
+       }
+   },
+   '36.000 BTU/h': {
+       'R-32': {
+            'carga_por_metro': 25,
+            'limite_fabrica': 5.0,
+            'limite_minimo': 2.0,
+            'limite_maximo': 25.0,
+            'cabo_pp': '4 x 1,5mm',
+            'ligacao_eletrica': 'condensadora',
+            'tubulacao': '1/4 e 1/2',   
+       }
+   },   
+ },
+
 'Samsung': {
     '9 a 12.000 BTU/h': {
-        'R-32':{
+        'R-410a':{
             'carga_por_metro': 15,
             'limite_fabrica': 5.0,
             'limite_minimo': 3.0,
@@ -339,22 +504,22 @@ parametros = {
       },
   
     '18.000 BTU/h':{
-        'R-32':{
+        'R-410a':{
             'carga_por_metro':15,
             'limite_fabrica': 5.0,
             'limite_minimo': 3.0,
-            'limite_maximo': 15.0,
+            'limite_maximo': 300,
             'cabo_pp': '5 x 1,5mm',
             'ligacao_eletrica': 'condensadora',
             'tubulacao': '1/4 e 1/2'
     }  
   },
     '22.000 BTU/h':{
-        'R-32':{
+        'R-410a':{
             'carga_por_metro':15,
             'limite_fabrica': 5.0,
             'limite_minimo': 3.0,
-            'limite_maximo': 15.0,
+            'limite_maximo': 30.0,
             'cabo_pp': '5 x 1,5mm',
             'ligacao_eletrica': 'condensadora',
             'tubulacao': '1/4 e 5/8'
@@ -363,8 +528,8 @@ parametros = {
 },
 
 'TCL':{
-    '9 a 12.000 BTU/h':{
-        'R-32':{
+    '9.000 BTU/h':{
+        'R-410a':{
             'carga_por_metro': 15.0,
             'limite_fabrica': 3.0,
             'limite_minimo': 2.0,
@@ -374,6 +539,26 @@ parametros = {
             'tubulacao': '1/4 e 3/8'
             }
     },
+    '12.000 BTU/h':{
+        'R-32':{
+            'carga_por_metro': 15.0,
+            'limite_fabrica': 3.0,
+            'limite_minimo': 2.0,
+            'limite_maximo': 15.0,
+            'cabo_pp': '4 x 1,5mm',
+            'ligacao_eletrica': 'condensadora',
+            'tubulacao': '1/4 e 3/8',
+            },
+         'R-410a':{
+            'carga_por_metro': 20.0,
+            'limite_fabrica': 3.0,
+            'limite_minimo': 2.0,
+            'limite_maximo': 15.0,
+            'cabo_pp': '4 x 1,5mm',
+            'ligacao_eletrica': 'condensadora',
+            'tubulacao': '1/4 e 3/8',
+            },  
+    },
     '18.000 BTU/h':{
         'R-32':{
             'carga_por_metro': 25.0,
@@ -382,10 +567,18 @@ parametros = {
             'limite_maximo': 15.0,
             'cabo_pp': '4 x 1,5mm',
             'ligacao_eletrica': 'condensadora',
-            'tubulacao': '1/4 e 3/8'
-            }
-    },
-    
+            'tubulacao': '1/4 e 3/8',
+            },       
+        'R-410a':{
+            'carga_por_metro': 30.0,
+            'limite_fabrica': 3.0,
+            'limite_minimo': 2.0,
+            'limite_maximo': 15.0,
+            'cabo_pp': '4 x 1,5mm',
+            'ligacao_eletrica': 'condensadora',
+            'tubulacao': '1/4 e 3/8',
+            },     
+    },  
     '24.000 BTU/h':{
         'R-32':{
             'carga_por_metro': 25.0,
@@ -394,18 +587,38 @@ parametros = {
             'limite_maximo': 15.0,
             'cabo_pp': '4 x 1,5mm',
             'ligacao_eletrica': 'condensadora',
-            'tubulacao': '1/4 e 1/2'
+            'tubulacao': '1/4 e 1/2',
+            },
+        'R-410a':{
+            'carga_por_metro': 30.0,
+            'limite_fabrica': 3.0,
+            'limite_minimo': 2.0,
+            'limite_maximo': 15.0,
+            'cabo_pp': '4 x 1,5mm',
+            'ligacao_eletrica': 'condensadora',
+            'tubulacao': '1/4 e 1/2',
+            },       
+    }, 
+    '32.000 BTU/h':{
+        'R-410a':{
+            'carga_por_metro': 30.0,
+            'limite_fabrica': 3.0,
+            'limite_minimo': 2.0,
+            'limite_maximo': 15.0,
+            'cabo_pp': '4 x 1,5mm',
+            'ligacao_eletrica': 'condensadora',
+            'tubulacao': '1/4 e 1/2',
             }
-    },      
-  }
+    },       
+  },
 }
 
 fabricante = st.selectbox('**Fabricante**', list(parametros.keys()))
 capacidades = list(parametros[fabricante].keys())
 capacidade = st.selectbox('**Capacidade**', capacidades)
-tipo_gas = st.selectbox('**Fluido Refrigerante**', ['R-32', 'R-410A', 'R-22'])
+tipos_gas = list(parametros[fabricante][capacidade].keys())
+tipo_gas = st.selectbox('**Fluido Refrigerante**', tipos_gas)
 metros_tubulacao = st.number_input('**Digite o comprimento total da instalação**', min_value=0.0, step=0.5)
-
 
 # Verificando se os parâmetros selecionados existem
 dados = parametros.get(fabricante, {}).get(capacidade, {}).get(tipo_gas)
@@ -419,7 +632,13 @@ if dados:
     st.write(f"**Cabo PP:** {dados['cabo_pp']}")
     st.write(f"**Ligação elétrica:** {dados['ligacao_eletrica']}")
 
+    # Exibir observações, se houver
+    if 'observacoes' in dados:
+        st.info(f'**Observações:** {dados['observacoes']}')
+    
     # Verificando se a distância da tubulação excede o limite máximo
+    if metros_tubulacao < dados['limite_minimo']:
+        st.warning("O comprimento mínimo da tubulação é menor do que o permitido. Verifique a distância mínima.")
     if metros_tubulacao > dados['limite_maximo']:
         st.warning("A distância da tubulação excede o limite máximo permitido.")
     elif metros_tubulacao > dados['limite_fabrica']:
